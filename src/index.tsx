006449f64a4dd4948668c233d5b4767fa6b2361e
Zeedkey <the.furriest.man.ever@gmail.com>
2022-09-02 14:12:06 +0400

    :sparkles: Apollo logic and index file added

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "98.css";
-import "@styles/main.sass";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Auth } from "./pages/auth";
-import { Chat } from "./pages/chat";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '98.css';
+import '@styles/main.sass';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Auth } from './pages/auth';
+import { Chat } from './pages/chat';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Auth />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
