d6ecc8c02390fbd11cc435bd602cc732025ffbf9
Zeedkey <the.furriest.man.ever@gmail.com>
2022-09-05 13:25:11 +0400

    :hammer: Message refactored

--- src/common/message/index.tsx
+++ src/common/message/index.tsx
@@ -1,11 +1,13 @@
-import styles from './message.module.sass'
+import styles from './message.module.sass';
 
 export const Message: React.FC = () => {
-    return(
-        <div className={styles.message}>
-            <h2 className={styles.message__createdAt}>[11:55 22.02.2022]</h2>
-            <h2 className={styles.message__author}>Wannega</h2>
-            <h2 className={styles.message__content}>Fuck the police</h2>
-        </div>
-    )
-}
+  return (
+    <div className={styles.message}>
+      <h2 className={styles.message__createdAt}>
+        [11:55 22.02.2022]
+      </h2>
+      <h2 className={styles.message__author}>Wannega</h2>
+      <h2 className={styles.message__content}>Message</h2>
+    </div>
+  );
+};
