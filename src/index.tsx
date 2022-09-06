a353298e85045be7e2d2dda51f3e77f496631477
Zeedkey <the.furriest.man.ever@gmail.com>
2022-09-06 14:27:44 +0400

    :hammer: Graphql port fixed

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import {
 } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: 'http://localhost:3001/graphql',
   cache: new InMemoryCache(),
 });
 
