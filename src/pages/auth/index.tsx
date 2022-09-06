7792f5f876d3e956006c7fbbbade5c4a2439c719
Zeedkey <the.furriest.man.ever@gmail.com>
2022-09-06 14:28:50 +0400

    :sparkles: Auth page overhauled

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,42 +1,44 @@
-import { Window } from '@common';
 import styles from './auth.module.sass';
+import { Window } from '@common';
+import { useState } from 'react';
+
+const serverTitle = 'Server status';
+const authTitle = 'IRC [Auth]';
 
 export const Auth: React.FC = () => {
+  const [isRegMode, setRegMode] = useState(false);
+  
   return (
-    <main className={styles.auth}>
-      <section className={styles.auth__enter}>
-        <Window title="IRC [Auth]">
-          <div className={styles.auth__welcome}>
-            <h5>Welcome to the IRC-client</h5>
+    <main>
+      <section className={styles.server}>
+        <Window title={serverTitle}>
+          <div className={styles.server__body}>
+            <ul className={styles.server__list}>
+              <li>STATUS: ONLINE</li>
+              <li>USERS ONLINE: 6</li>
+              <li>ACTIVITY: High</li>
+            </ul>
           </div>
-          <form className={styles.auth__form}>
-            <div className="field-row-stacked">
-              <label htmlFor="login">Login</label>
-              <input id="login" type="text" maxLength={16} />
-            </div>
-            <div className="field-row-stacked">
-              <label htmlFor="password">Password</label>
-              <input id="password" type="password" />
-            </div>
-            <button type="submit">Submit</button>
-          </form>
         </Window>
       </section>
-
-      <section className={styles.auth__servers}>
-        <Window title="Server status">
-          <ul>
-            <li>
-              Connectivity: <span>STABLE</span>
-            </li>
-            <li>Users online: 6</li>
-            <li>
-              Status: <span>operational</span>
-            </li>
-            <li>
-              Message sending frequency: <span>RARE</span>
-            </li>
-          </ul>
+      <section className={styles.auth}>
+        <Window title={authTitle}>
+          <div className={styles.auth__welcome}>
+            <p className={styles.auth__title}>
+              Welcome to the IRC chat client.
+              <span className={styles.auth__authtype}>
+                Please, login
+              </span>
+            </p>
+          </div>
+          <form className={styles.auth__body}>
+            <input placeholder="Enter your login" />
+            <input placeholder="Enter your password" />
+            <input type="submit" value="Submit" />
+          </form>
+          <button>
+            <p>No account? Create a new one...</p>
+          </button>
         </Window>
       </section>
     </main>
